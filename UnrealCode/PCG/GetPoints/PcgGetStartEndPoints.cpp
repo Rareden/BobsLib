37b89a050114984e3f7b99365eec8d765686a8e9
rare <incooperm@gmail.com>
2025-01-24 13:32:21 +1300

    Points tool

---
+++ UnrealCode/PCG/GetPoints/PcgGetStartEndPoints.cpp
@@ -0,0 +1,88 @@
+﻿// Copyright 2025 Helm Boy Studios . All rights reserved
+
+
+#include "PcgGetStartEndPoints.h"
+#include "PCGContext.h"
+#include "PCGPin.h"
+#include "Data/PCGPointData.h"
+
+UGetPointContext::UGetPointContext(): Mode(EGetPointMode::Last)
+{
+}
+
+void UGetPointContext::PostLoad()
+{
+	Super::PostLoad();
+}
+#if WITH_EDITOR
+void UGetPointContext::PostDuplicate(EDuplicateMode::Type DuplicateMode)
+{
+	
+}
+
+void UGetPointContext::PostDuplicate(bool bDuplicateForPIE)
+{
+	Super::PostDuplicate(bDuplicateForPIE);
+}
+#endif
+TArray<FPCGPinProperties> UGetPointContext::InputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(TEXT("In"), EPCGDataType::Point);
+	return PinProperties;
+	
+}
+
+TArray<FPCGPinProperties> UGetPointContext::OutputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(TEXT("Out"), EPCGDataType::Point);
+	return PinProperties;
+}
+
+FPCGElementPtr UGetPointContext::CreateElement() const
+{
+	return MakeShared<FGetPointElement>();
+}
+
+bool FGetPointElement::ExecuteInternal(FPCGContext* Context) const
+{
+	if (!Context) return false;
+	TArray<FPCGTaggedData> Inputs = Context->InputData.GetInputs();
+	if (Inputs.IsEmpty() )
+		return true;
+	const UGetPointContext* Settings = Context->GetInputSettings<UGetPointContext>();
+	// Create output handle
+	FPCGTaggedData& Output = Context->OutputData.TaggedData.Add_GetRef(FPCGTaggedData());
+	TArray<FPCGTaggedData> Input =	Context->InputData.GetInputsByPin(TEXT("In"));
+	
+	// Create new point data
+	UPCGPointData* OutputData = NewObject<UPCGPointData>();
+	Output.Data = OutputData;
+
+	// iterator for the multiple data set pins that cn be connected
+	for (const FPCGTaggedData& TaggedData : Input)
+	{
+		UPCGPointData* PointData = Cast<UPCGPointData>(TaggedData.Data);
+		if(IsValid(PointData) && PointData->GetPoints().Num() > 0)
+		{
+			// get last for first, depending on setting
+			switch ( Settings->Mode)
+			{
+				case Last:
+						OutputData->GetMutablePoints().Add(PointData->GetPoints().Last());
+					break;
+				case First:
+						OutputData->GetMutablePoints().Add(PointData->GetPoints()[0]);
+					break;
+				case StartAndEnd:
+						OutputData->GetMutablePoints().Add(PointData->GetPoints().Last());
+						OutputData->GetMutablePoints().Add(PointData->GetPoints()[0]);
+					break;
+			}
+		}
+	}
+
+
+	return true;
+}
---
+++ UnrealCode/PCG/GetPoints/PcgGetStartEndPoints.h
@@ -0,0 +1,56 @@
+﻿// Copyright 2025 Helm Boy Studios . All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGElement.h"
+#include "PCGSettings.h"
+#include "UObject/Object.h"
+#include "PcgUtils.generated.h"
+
+UENUM(BlueprintType)
+enum EGetPointMode : uint8
+{
+	Last,
+	First,
+	StartAndEnd,
+};
+/* Boiler plate class for the Graph UI */
+UCLASS(BlueprintType, ClassGroup = (Procedural))
+class INSERTYOURMODULENAME_API UGetPointContext : public UPCGSettings
+{
+	GENERATED_BODY()
+public:
+	UGetPointContext();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Settings)
+	TEnumAsByte<EGetPointMode> Mode;
+	
+	//~Begin UObject interface
+	virtual void PostLoad() override;
+	//~End UObject interface
+
+	//~Begin UPCGSettings interface
+#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override { return "GetStartOrLastPointContext"; }
+	virtual FText GetDefaultNodeTitle() const override { return NSLOCTEXT("PCGBaseNodeSettings", "NodeTitle", "Get Start Or Last Point"); }
+	virtual FText GetNodeTooltipText() const override { return FText::FromString("Gets the Start or Last point of a point data set"); }
+	virtual FLinearColor GetNodeTitleColor() const override { return FLinearColor::Gray; }
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Filter; }
+	virtual void PostDuplicate(EDuplicateMode::Type DuplicateMode) override;
+	virtual void PostDuplicate(bool bDuplicateForPIE) override;
+#endif
+
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override ;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	
+protected:
+	// makes a FGetPointElement 
+	virtual FPCGElementPtr CreateElement() const override;
+};
+
+class FGetPointElement  : public FSimplePCGElement
+{
+protected:
+	// does the actual thing
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+};
